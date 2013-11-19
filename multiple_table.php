6f148f9f3cae3c60a6856ee188f49b3179c23ca6
cto9 <cto9dp@aol.com>
2013-11-19 21:04:46 +0700

    new version

--- multiple_table.php
+++ multiple_table.php
@@ -1,35 +1,45 @@
 <form action="" method="get">
-    <p>Width <input type="number" name="width" /></p>
+    <p>Width <input type="number" name="width" /><?php if(isset($_GET['width']) && !is_get_variable_valid('width',0,1000)) echo 'error number' ?></p>
     <p>Height <input type="number" name="height" /></p>
     <p><input type="submit" /></p>
 </form>
 
 <?php
 
-    if((isset($_GET['width']) && isset($_GET['height']))){
-        if($_GET['width'] > 0 && $_GET['width'] < 1000 && $_GET['height'] > 0 && $_GET['height'] < 1000){
-            $width = $_GET['width'];
-            $height = $_GET['height'];
-        }
+    function is_get_variable_valid($varName, $minValue, $maxValue){
+        return isset($_GET[$varName] && $_GET[$varName] > $minValue && $_GET[$varName] < $maxValue);
+    }
+
+
+    if(is_get_variable_valid('width',0,1000) && is_get_variable_valid('height',0,1000)){
+        $width = $_GET['width'];
+        $height = $_GET['height'];
     }
     else{
         $width = 10;
         $height = 10;
     }
 
-    echo "<table>";
-    for($i = 1; $i <= $width; $i++){
-        echo "<tr>";
-        for($j = 1; $j <=$height; $j++){
+    echo '<table>';
+
+    echo '<tr>';
+    echo '<td>*</td>';
+    for($j = 1; $j <= $width){
+        echo '<td>'.$j.'</td>';
+    }
+    echo '</tr>';
+
+    for($i = 2; $i <= $height; $i++){
+        echo '<tr>';
+        for($j = 1; $j <=$width; $j++){
 
             if(($i == 1) || ($j == 1)){
                 echo '<td>'.($i * $j).'</td>';
             }
-            else if($j % 2){
-                echo '<td bgcolor="grey">'.($i * $j).'</td>';
-            }
-            else
-                echo '<td>'.($i * $j).'</td>';
+            else{?>
+                <td <?php if($j % 2 == 0) echo 'bgcolor="grey"'?>> <?php echo ($i*$j) ?> </td>;
+            <?php } ?>
+
         }
         echo "</tr>";
     }
