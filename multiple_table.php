024091c28c44912f8c2af0ac487ca621df295bac
cto9 <cto9dp@aol.com>
2013-11-19 12:53:15 +0700

    form created

---
+++ multiple_table.php
@@ -0,0 +1,10 @@
+<form action="" method="get">
+    <p>Width <input type="number" name="width" /></p>
+    <p>Height <input type="number" name="height" /></p>
+    <p><input type="submit" /></p>
+</form>
+
+<?php
+    $width = $_GET['width'];
+    $height = $_GET['height'];
+?>
