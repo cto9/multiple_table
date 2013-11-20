23df8a2991e774c5fc487ea19fff970ad5112c61
cto9 <cto9dp@aol.com>
2013-11-20 16:21:40 +0700

    added some tags etc

--- tasksproject/index.php
+++ tasksproject/index.php
@@ -1,10 +1,23 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Tasks Project</title>
+</head>
+
+<body>
+
 <form action="" method="get">
-    <p>Task Name <input type="text" name="taskname" /></p>
-    <p>Description <input type="textarea" name="description" /></p>
-    <p>Task Type <select name="tasktype">
+    <p><label for="taskname">Название задания</label> <input id="taskname" type="text" name="taskname" /></p>
+    <p><label for="description">Описание</label> <textarea id="description" name="description" cols=60 rows=10></textarea></p>
+    <p><label for="tasktype">Тип задания</label> <select id="tasktype" name="tasktype">
             <option value="development">Development
             <option value="planning">Planning
             <option value="debugging">Debugging
             </select></p>
     <p><input type="submit" /></p>
 </form>
+
+</body>
+
+</html>
