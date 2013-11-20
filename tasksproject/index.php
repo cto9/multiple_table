b43e0c0d4dec8d46f441b02099c2072815fefc02
cto9 <cto9dp@aol.com>
2013-11-20 15:13:30 +0700

    form for tasks

---
+++ tasksproject/index.php
@@ -0,0 +1,10 @@
+<form action="" method="get">
+    <p>Task Name <input type="text" name="taskname" /></p>
+    <p>Description <input type="textarea" name="description" /></p>
+    <p>Task Type <select name="tasktype">
+            <option value="development">Development
+            <option value="planning">Planning
+            <option value="debugging">Debugging
+            </select></p>
+    <p><input type="submit" /></p>
+</form>
