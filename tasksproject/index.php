41b7944c0b10058a601613b4f261dea00ae0b49e
cto9 <cto9dp@aol.com>
2013-11-22 21:22:02 +0700

    final version

--- tasksproject/index.php
+++ tasksproject/index.php
@@ -8,46 +8,130 @@
 
 <body>
 
-<form action="" method="post">
-    <p><label for="taskname">Название задания</label> <input id="taskname" type="text" name="taskname" /></p>
-    <p><label for="description">Описание</label> <textarea id="description" name="description" cols=60 rows=10></textarea></p>
-    <p><label for="tasktype">Тип задания</label> <select id="tasktype" name="tasktype">
+<?php
+
+function getDataFromRequest($datapost)
+{
+    $taskname = empty($datapost['taskname']) ? "" : trim($datapost['taskname']);
+    $description = empty($datapost['taskname']) ? "" : trim($datapost['description']);
+    $tasktype = $datapost['tasktype'];
+    return array('taskname' => $taskname, 'description' => $description, 'tasktype' => $tasktype);
+}
+
+function validateData($data, $db)
+{
+
+    $validationRes = array('success' => true, 'messages' => array());
+    $taskTypeVariants = array('development', 'planning', 'debugging');
+
+    if( empty($data['taskname']) ) {
+        addErrorMessage($validationRes, 'Название задачи не может быть пустым');
+    }
+
+    $nameValid = $db->prepare('SELECT COUNT(*) FROM tasks WHERE taskname = :taskname');
+    $nameValid->execute(array('taskname' => $data['taskname']));
+    $checkUniqName = $nameValid->fetch(PDO::FETCH_ASSOC);
+
+    if( $checkUniqName['COUNT(*)'] == 1 ) {
+        addErrorMessage($validationRes, sprintf('Задание с именем %s уже существует', $data['taskname']));
+    }
+
+    if( !in_array($data['tasktype'], $taskTypeVariants) ) {
+        addErrorMessage($validationRes, 'Неверный тип задания');
+    }
+
+    return $validationRes;
+}
+
+function addErrorMessage(&$validationResult, $message)
+{
+    $validationResult['success'] = false;
+    $validationResult['messages'][] = array('type' => 'Error', 'text' => $message);
+}
+
+function insertTask($data, $db)
+{
+    $st = $db->prepare('INSERT INTO tasks( creationdate, taskname, description, tasktype ) VALUES( NOW(), :taskname, :description, :tasktype )');
+    $st->execute(array(':taskname' => $data['taskname'], ':description' => $data['description'], ':tasktype' => $data['tasktype']));
+
+    $insertId = $db->lastInsertId();
+    return array('type' => 'success', 'message' => sprintf('Задание с id = %s вставлено в БД', $insertId));
+}
+
+$host = "localhost";
+$user = "root";
+$password = "root";
+$database = "tasksproject";
+
+try {
+    $db = new PDO(sprintf('mysql:host=%s;dbname=%s', $host, $database), $user, $password);
+} catch (PDOException $e) {
+    echo '<span class="error_message">Error!: ' . $e->getMessage() . "<br/>";
+    die();
+}
+
+if( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+
+    $data = getDataFromRequest($_POST);
+    $validationResult = validateData($data, $db);
+
+    $messageOut = array();
+
+    if( $validationResult['success'] == true ) {
+        $messageOut[] = insertTask($data, $db);
+    } else {
+        $messageOut = $validationResult['messages'];
+    }
+
+    foreach ($messageOut as $messages) {
+//        echo '<span class="error', $messages['type'], '">', $messages['text'], '</span>';
+        echo '<span class = "error_message">', $messages['type'], ': ', $messages['text'], '</span>';
+    }
+
+
+}
+
+?>
+
+
+<table>
+
+    <form action="" method="post">
+    <tr><td><label for="taskname">Название задания</label></td> <td><input id="taskname" type="text" name="taskname"/></td></tr>
+
+    <tr><td><label for="description">Описание</label></td> <td><textarea id="description" name="description" cols=60 rows=10></textarea></td></tr>
+
+    <tr><td><label for="tasktype">Тип задания</label></td> <td><select id="tasktype" name="tasktype">
             <option value="development">Development
             <option value="planning">Planning
             <option value="debugging">Debugging
-            </select></p>
-    <p><input type="submit" /></p>
+        </select></td>
+     </tr>
+    <tr><td><input type="submit"/></td></tr>
 </form>
 
-<?php
+</table>
 
-    $host = "localhost";
-    $user = "root";
-    $password = "root";
-//    $db = "tasksproject";
-
-    try {
-        $db = new PDO('mysql:host=localhost;dbname=tasksproject', $user, $password);
-//        $dbh = null;
-    } catch (PDOException $e) {
-        echo '<span class="error_message">Error!: ' . $e->getMessage() . "<br/>";
-        die();
+<table border="1">
+    <tr>
+        <td>Дата создания</td>
+        <td>Название задания</td>
+        <td>Описание</td>
+        <td>Тип задания</td>
+    </tr>
+    <?php
+    $stmt = $db->query('SELECT * FROM tasks ORDER BY creationdate DESC');
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        echo '<tr>';
+        echo '<td>' . $row['creationdate'] . '</td>';
+        echo '<td>' . $row['taskname'] . '</td>';
+        echo '<td>' . $row['description'] . '</td>';
+        echo '<td>' . $row['tasktype'] . '</td>';
+        echo '</tr>';
     }
+    ?>
 
-    if(isset($_POST['taskname']))
-        $taskname = $_POST['taskname'];
-    if(isset($_POST['description']))
-        $description = $_POST['description'];
-    if(isset($_POST['tasktype']))
-        $tasktype = $_POST['tasktype'];
-
-//    $querry = "INSERT INTO tasks VALUES (NOW(), $taskname, $description, $tasktype)";
-
-$st = $db->prepare('INSERT INTO tasks( creationdate, taskname, description, tasktype ) VALUES( NOW(), :taskname, :description, :tasktype )');
-$st->execute(array( ':taskname' => $taskname, ':description' => $description, ':tasktype' => $tasktype ));
-
-
-?>
+</table>
 
 
 </body>
---
+++ tasksproject/info.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: danny
+ * Date: 11/22/13
+ * Time: 1:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+phpinfo();
