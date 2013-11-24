d32039dc0d5a1e229e85d5789244cdb5e5b3b49f
cto9 <cto9dp@aol.com>
2013-11-24 18:23:56 +0700

    show data from db per pages

--- tasksproject/index.php
+++ tasksproject/index.php
@@ -84,7 +84,6 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
     }
 
     foreach ($messageOut as $messages) {
-//        echo '<span class="error', $messages['type'], '">', $messages['text'], '</span>';
         echo '<span class = "error_message">', $messages['type'], ': ', $messages['text'], '</span>';
     }
 
@@ -107,7 +106,7 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
             <option value="debugging">Debugging
         </select></td>
      </tr>
-    <tr><td><input type="submit"/></td></tr>
+    <tr><td></td><td align="right"><input type="submit"/></td></tr>
 </form>
 
 </table>
@@ -120,7 +119,52 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
         <td>Тип задания</td>
     </tr>
     <?php
-    $stmt = $db->query('SELECT * FROM tasks ORDER BY creationdate DESC');
+
+    $messagesPerPage = 10;
+//    $page = $_GET['page'];
+//    $page = 2;
+    $page = empty($_GET['page']) ? 1 : $_GET['page'];
+
+    $tmp = $db->query('SELECT * FROM tasks');
+    $row_count = $tmp->rowCount();
+
+    $totalPages = intval(($row_count - 1) / $messagesPerPage) + 1;
+
+    if(empty($page) || $page < 0) $page = 1;
+    if($page > $totalPages) $page = $totalPages;
+
+    $start = $page * $messagesPerPage - $messagesPerPage;
+
+    $stmt = $db->query(sprintf('SELECT * FROM tasks ORDER BY creationdate DESC LIMIT %s, %s', $start, $messagesPerPage));
+    while($postrow = $stmt->fetch(PDO::FETCH_ASSOC)){
+        echo '<tr>';
+        echo '<td>' . $postrow['creationdate'] . '</td>';
+        echo '<td>' . $postrow['taskname'] . '</td>';
+        echo '<td>' . $postrow['description'] . '</td>';
+        echo '<td>' . $postrow['tasktype'] . '</td>';
+        echo '</tr>';
+    }
+
+
+/*    for($i = 0; $i < $messagesPerPage; $i++)
+    {
+        echo "<tr>
+         <td>".$postrow[$i]['creationdate']."</td>
+         <td>".$postrow[$i]['taskname']."</td>
+         <td>".$postrow[$i]['description']."</td>
+         <td>".$postrow[$i]['tasktype']."</td></tr>";
+    }
+*/
+
+    /* code for insert 100 tasks for test db
+        $data['tasktype'] = 'planning';
+        $data['description'] = 'qwertty';
+        for($i=0;$i<100;$i++){
+            $data['taskname'] = 'TaskNumber'.$i;
+            $messageOut[] = insertTask($data,$db);
+        }
+    */
+/*    $stmt = $db->query('SELECT * FROM tasks ORDER BY creationdate DESC');
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
         echo '<tr>';
         echo '<td>' . $row['creationdate'] . '</td>';
@@ -129,10 +173,34 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
         echo '<td>' . $row['tasktype'] . '</td>';
         echo '</tr>';
     }
+*/
     ?>
 
 </table>
 
+<?php
+//    if($page != 1) $firstPage = '<a href=?page=1>FirstPage</a>';
+    $firstPage = '<a href=?page=1>FirstPage</a>';
+    if($page != $totalPages) $lastPage = '<a href=?page='.$totalPages.'>LastPage</a>';
+
+    if($page+5 > $totalPages){
+
+        echo $firstPage.'<b>'.$page.'</b>';
+        for($i = $page; $i < $totalPages; $i++){
+            echo ' | <a href=?page='.($i+1).'>'.($i+1).'</a>';
+        }
+        echo $lastPage;
+    }
+    else{
+
+    $next5Pages = ' | <a href=?page='.($page+5).'>'.($page+5).'</a>';
+    $next4Pages = ' | <a href=?page='.($page+4).'>'.($page+4).'</a>';
+    $next3Pages = ' | <a href=?page='.($page+3).'>'.($page+3).'</a>';
+    $next2Pages = ' | <a href=?page='.($page+2).'>'.($page+2).'</a>';
+    $next1Pages = ' | <a href=?page='.($page+1).'>'.($page+1).'</a>';
+    echo $firstPage.'<b>'.$page.'</b>'.$next1Pages.$next2Pages.$next3Pages.$next4Pages.$next5Pages.$lastPage;
+    }
+?>
 
 </body>
 
